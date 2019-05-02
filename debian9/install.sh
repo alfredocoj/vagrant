f86ac71d65c45c4669b29c0f24e147e79a941661
Alfredo Oliveira <alfredo.coj@gmail.com>
2019-05-02 18:12:53 -0300

    install script.

--- debian9/install.sh
+++ debian9/install.sh
@@ -47,57 +47,6 @@ sudo ln -s /usr/share/zoneinfo/America/Fortaleza /etc/localtime
 
 printf "\n\rINICIANDO SCRIPT DE INSTALAÇÃO PARA DESENVOLVEDORES DA IT HAPPENS - GRUPO MATEUS ... \n\r"
 
-# atualização dos indices dos pacotes
-if ! sudo apt update -y
-then
-    printf "Não foi possível atualizar os repositórios. Verifique seu arquivo /etc/apt/sources.list\n\r"
-    exit 1
-fi
-
-echo "Atualização de repositório feita com sucesso\n\r"
-
-
-# compiladores do c++
-if ! sudo apt install build-essential -y
-then
-    printf "Não foi possível instalar o pacote build-essential\n\r"
-    exit 1
-fi
-printf "Instalação dos compilador do c++ finalizada\n\r"
-
-
-# driver para conectar ao sql server, postgresql
-if ! sudo apt install unixodbc unixodbc-dev freetds-dev freetds-bin tdsodbc postgresql-server-dev-all -y
-then
-    printf "Não foi possível instalar os drivers para conectar ao sql server, postgresql\n\r"
-    exit 1
-fi
-printf "Instalação dos drivers sqlserver, postgresql finalizada...\n\r"
-
-# stack trace do compilador do c++
-if ! sudo apt install libx11-xcb-dev libdw1 libdw-dev -y
-then
-    printf "Não foi possível instalar as libs libdw1 libdw-dev\n\r"
-    exit 1
-fi
-printf "Instalação das libs libdw1 libdw-dev finalizada...\n\r"
-
-# libs extras
-if ! sudo apt install libcurl4-openssl-dev -y
-then
-    printf "Não foi possível instalar as bibliotecas libcurl4-openssl-dev\n\r"
-    exit 1
-fi
-printf "Instalação das bibliotecas libcurl4-openssl-dev finalizada...\n\r"
-
-# bibliotecas para exibição de gráficos opengl para o qtcreator funcionar a compilação
-if ! sudo apt install libgl1-mesa-dev -y
-then
-    printf "Não foi possível instalar as bibliotecas para exibição gráficas\n\r"
-    exit 1
-fi
-printf "Instalação das bibliotecas para exibição gráficas finalizada...\n\r"
-
 # instalar o icecc - compilação distribuida
 if ! sudo apt install icecc -y
 then
@@ -136,15 +85,5 @@ then
 fi
 printf "Instalação do git htop...\n\r"
 
-echo "Configuração do IECC iniciada...\n\r"
-sudo sed -i 's/ICECC_NETNAME=""/ICECC_NETNAME="node"/' /etc/icecc/icecc.conf
-
-
-sudo sed -i 's/ICECC_SCHEDULER_HOST=""/ICECC_SCHEDULER_HOST="icecc.mateus"/' /etc/icecc/icecc.conf
-echo "Configuração do IECC finalizado...\n\r"
-
-echo "Configuração do Drive ODBC iniciada...\n\r"
-echo -e "[freetds]\r\ndescription    = v0.63 with protocol v8.0\r\ndriver    = /usr/lib/x86_64-linux-gnu/odbc/libtdsodbc.so\r\nsetup    = /usr/lib/x86_64-linux-gnu/odbc/libtdss.so\r\nusagecount    = 1" >> /etc/odbcinst.ini
-echo "Configuração do Drive ODBC finalizada...\n\r"
 
 printf "\n\rSCRIPT FINALIZADO ... \n\r"
